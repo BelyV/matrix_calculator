a37bb9c451e85bfa367be250685e10f20e326f88
BelyV <127022124+BelyV@users.noreply.github.com>
2023-03-12 14:39:12 +0100

    Poprawki w formatowaniu w main

--- main.cpp
+++ main.cpp
@@ -61,77 +61,82 @@ int main() {
     int wierszA, kolumnaA = 0, wierszB, kolumnaB;
     char w;
     string f;
+
     cout<<"Dla +,-,* nacisnij - 1"<<endl;
     cout<<"Dla transponowania nacisnij - 2"<<endl;
+
     cin>>f;
     cout<<endl;
 
-    if(f=="1"){
+    if(f=="1")
+    {
+        cout << "Podaj ilosc wierszej i kolumn dla macierzy A: ";
+        cin >> wierszA >> kolumnaA;
+        dodajmacierz(a, wierszA, kolumnaA);
+        wypiszmacirz(a, wierszA, kolumnaA);
 
-    cout << "Podaj ilosc wierszej i kolumn dla macierzy A: ";
-    cin >> wierszA >> kolumnaA;
-    dodajmacierz(a, wierszA, kolumnaA);
-    wypiszmacirz(a, wierszA, kolumnaA);
+        cout << "Podaj ilosc wierszej i kolumn dla macierzy B: ";
+        cin >> wierszB >> kolumnaB;
+        dodajmacierz(b, wierszB, kolumnaB);
+        wypiszmacirz(b, wierszB, kolumnaB);
 
-    cout << "Podaj ilosc wierszej i kolumn dla macierzy B: ";
-    cin >> wierszB >> kolumnaB;
-    dodajmacierz(b, wierszB, kolumnaB);
-    //if (wierszA != wierszB || kolumnaA != kolumnaB) {
-      //  cout << "Macirzy A i B musza byc tego samego rozmiaru dla dodawania i odejmowania.\n";
-    //}
-     wypiszmacirz(b, wierszB, kolumnaB);
+        cout<<"Podaj + - dodawanie,- - odejmowanie,* - mnorzenie "<<endl;
+        cin >>w;
 
-     cout<<"Podaj + - dodawanie,- - odejmowanie,* - mnorzenie "<<endl;
-     cin >>w;
-     if(w=='+' || w=='-'){
-    if (wierszA != wierszB || kolumnaA != kolumnaB) {
-        cout << "Macirzy A i B musza byc tego samego rozmiaru dla dodawania i odejmowania.\n";
-    } else {
-        //cout<<"Podaj + lub - "<<endl;
-        //cin >>w;
-        if(w=='+'){
+        if(w=='+' || w=='-')
+        {
+            if (wierszA != wierszB || kolumnaA != kolumnaB)
+            {
+                cout << "Macirzy A i B musza byc tego samego rozmiaru dla dodawania i odejmowania.\n";
+            }
+            else
+            {
+                if(w=='+')
+                {
+                    dodawanie(a, b, c, wierszA, kolumnaA);
+                    wypiszmacirz(c, wierszA, kolumnaA);
+                }
+                else
+                    if(w=='-')
+                    {
+                        odejmowanie(a, b, c, wierszA, kolumnaA);
+                        wypiszmacirz(c, wierszA, kolumnaA);
+                    }
+                    else cout <<"Blad danych"<<endl;
+             }
+        }
+        else
+            if (w=='*')
+            {
+                if(wierszB==kolumnaA)
+                {
+                    mnorzenie(a, b, c, wierszA, kolumnaA, kolumnaB);
+                    wypiszmacirz(c, wierszA, kolumnaB);
 
-        dodawanie(a, b, c, wierszA, kolumnaA);
-        wypiszmacirz(c, wierszA, kolumnaA);}
-       else if(w=='-'){
-        odejmowanie(a, b, c, wierszA, kolumnaA);
-        wypiszmacirz(c, wierszA, kolumnaA);}
-        else cout <<"Blad danych"<<endl;
-
-
-
+                }
+                else  cout <<"Brak danych"<<endl;
+            }
+            else cout <<"Brak danych"<<endl;
     }
-     }
-     else if (w=='*') {
-         if(wierszB==kolumnaA)
-         {
-
-
-    mnorzenie(a, b, c, wierszA, kolumnaA, kolumnaB);
-    wypiszmacirz(c, wierszA, kolumnaB);
-
-}
-         else  cout <<"Brak danych"<<endl;
-     }
-     else cout <<"Brak danych"<<endl;
-}
-else {
-        if(f =="2"){
+    else
+    {
+        if(f =="2")
+        {
             cout << "Podaj ilosc wierszej i kolumn dla macierzy A: ";
             cin >> wierszA >> kolumnaA;
-
             if  (wierszA != kolumnaA)
             {
-        cout << "Macirz A musi byc symetryczny.\n";
+                cout << "Macirz A musi byc symetryczny.\n";
             }
-    else  {
+            else
+            {
                 dodajmacierz(a, wierszA, kolumnaA);
                 wypiszmacirz(a, wierszA, kolumnaA);
-
-    transponowanie(a,c,wierszA,kolumnaA);
-             wypiszmacirz(c, wierszA, kolumnaA);
+                transponowanie(a,c,wierszA,kolumnaA);
+                wypiszmacirz(c, wierszA, kolumnaA);
             }
         }
     }
 
+return 0;
 }
