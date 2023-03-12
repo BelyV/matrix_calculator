c25d9803c16e3edba1239db03c19193c6898bd23
BelyV <127022124+BelyV@users.noreply.github.com>
2023-03-12 16:48:01 +0100

    cikl

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <list>
+
 using namespace std;
 
 const int m = 10; // max pomiar
@@ -8,7 +10,17 @@ void dodajmacierz(int mat[][m], int wiersz, int kolumna) {
     cout << "Podaj elemienty macierzy " << wiersz << "x" << kolumna << ":\n";
     for (int i = 0; i < wiersz; i++) {
         for (int j = 0; j < kolumna; j++) {
-            cin >> mat[i][j];
+            //cin >> mat[i][j];
+            omt:
+            string s1;
+            cin >> s1;
+            for (int i = 0; i < s1.length(); i++){
+                if (((int(s1[i]) > 57) || (int(s1[i]) < 48)) && (int(s1[i]) != 45)){
+                    cout << "Podane bledne dane!" << endl;
+                    goto omt;
+                }
+            }
+            mat[i][j] = stoi(s1);
         }
     }
 }
@@ -21,6 +33,28 @@ void wypiszmacirz(int mat[][m], int wiersz, int kolumna) {
         }
         cout << endl;
     }
+    //wypisz_macierz_z_listy(macierz_to_list(mat[][m], wiersz, kolumna),wiersz,kolumna);
+}
+
+void wypisz_macierz_z_listy(list <int> list_mat, int wiersz, int kolumna){
+    cout << "Macierz "<< wiersz << "x" << kolumna << ":\n";
+    for (int i = 0; i < wiersz; i++) {
+        for (int j = 0; j < kolumna; j++) {
+            cout <<"["<< list_mat.front() <<"]"<< "\t";
+            list_mat.pop_front();
+        }
+        cout << endl;
+    }
+}
+
+list <int> macierz_to_list(int mat[][m], int wiersz, int kolumna){
+    list <int> list_mat;
+    for (int i = 0; i < wiersz; i++) {
+        for (int j = 0; j < kolumna; j++) {
+            list_mat.push_back(mat[i][j]);
+        }
+    }
+    return list_mat;
 }
 
 void dodawanie(int a[][m], int b[][m], int c[][m], int wiersz, int kolumna) {
@@ -97,19 +131,23 @@ int main() {
        //cin >> wierszB >> kolumnaB;
        //wypiszmacirz(b, wierszB, kolumnaB);
         fun1:
-        cout<<"Podaj + - dodawanie,- - odejmowanie,* - mnorzenie "<<endl;
+        cout<<"Podaj + - dodawanie,- - odejmowanie,* - mnorzenie, e-wyjsc "<<endl;
         cin >>w;
-
+        if (w=='e') {
+            return 0;
+        }
         if(w=='+' || w=='-') {
             cout << "Podaj ilosc wierszej i kolumn dla macierzy A: ";
             cin >> wierszA >> kolumnaA;
             dodajmacierz(a, wierszA, kolumnaA);
-            wypiszmacirz(a, wierszA, kolumnaA);
+            //wypiszmacirz(a, wierszA, kolumnaA);
+            wypisz_macierz_z_listy(macierz_to_list(a, wierszA, kolumnaA),wierszA,kolumnaA);
 
             cout << "Podaj ilosc wierszej i kolumn dla macierzy B: ";
             cin >> wierszB >> kolumnaB;
             dodajmacierz(b, wierszB, kolumnaB);
-            wypiszmacirz(b, wierszB, kolumnaB);
+            //wypiszmacirz(b, wierszB, kolumnaB);
+            wypisz_macierz_z_listy(macierz_to_list(b, wierszB, kolumnaB),wierszB,kolumnaB);
 
             if (wierszA != wierszB || kolumnaA != kolumnaB) {
                 cout << "Macirzy A i B musza byc tego samego rozmiaru dla dodawania i odejmowania.\n";
@@ -117,12 +155,14 @@ int main() {
             else {
                 if(w=='+') {
                     dodawanie(a, b, c, wierszA, kolumnaA);
-                    wypiszmacirz(c, wierszA, kolumnaA);
+                    //wypiszmacirz(c, wierszA, kolumnaA);
+                    wypisz_macierz_z_listy(macierz_to_list(c, wierszA, kolumnaA),wierszA,kolumnaA);
                 }
                 else
                     if(w=='-') {
                         odejmowanie(a, b, c, wierszA, kolumnaA);
-                        wypiszmacirz(c, wierszA, kolumnaA);
+                        //wypiszmacirz(c, wierszA, kolumnaA);
+                        wypisz_macierz_z_listy(macierz_to_list(c, wierszA, kolumnaA),wierszA,kolumnaA);
                     }
                     else cout <<"Blad danych"<<endl;
              }
@@ -132,16 +172,19 @@ int main() {
                 cout << "Podaj ilosc wierszej i kolumn dla macierzy A: ";
                 cin >> wierszA >> kolumnaA;
                 dodajmacierz(a, wierszA, kolumnaA);
-                wypiszmacirz(a, wierszA, kolumnaA);
+                //wypiszmacirz(a, wierszA, kolumnaA);
+                wypisz_macierz_z_listy(macierz_to_list(a, wierszA, kolumnaA),wierszA,kolumnaA);
 
                 cout << "Podaj ilosc wierszej i kolumn dla macierzy B: ";
                 cin >> wierszB >> kolumnaB;
                 dodajmacierz(b, wierszB, kolumnaB);
-                wypiszmacirz(b, wierszB, kolumnaB);
+                //wypiszmacirz(b, wierszB, kolumnaB);
+                wypisz_macierz_z_listy(macierz_to_list(b, wierszB, kolumnaB),wierszB,kolumnaB);
 
                 if(wierszB==kolumnaA) {
                     mnorzenie(a, b, c, wierszA, kolumnaA, kolumnaB);
-                    wypiszmacirz(c, wierszA, kolumnaB);
+                    //wypiszmacirz(c, wierszA, kolumnaB);
+                    wypisz_macierz_z_listy(macierz_to_list(c, wierszA, kolumnaB),wierszA,kolumnaB);
 
                 }
                 else  cout <<"Brak danych"<<endl;
@@ -160,9 +203,11 @@ int main() {
             }
             else {
                 dodajmacierz(a, wierszA, kolumnaA);
-                wypiszmacirz(a, wierszA, kolumnaA);
+                //wypiszmacirz(a, wierszA, kolumnaA);
+                wypisz_macierz_z_listy(macierz_to_list(a, wierszA, kolumnaA),wierszA,kolumnaA);
                 transponowanie(a,c,wierszA,kolumnaA);
-                wypiszmacirz(c, wierszA, kolumnaA);
+                //wypiszmacirz(c, wierszA, kolumnaA);
+                wypisz_macierz_z_listy(macierz_to_list(c, wierszA, kolumnaA),wierszA,kolumnaA);
             }
         }
     }
